88e3641680df7dc15138af65a2c1ecd5ce245014
Kevin Griffin <griffin.kev@gmail.com>
2022-12-01 11:42:19 -0500

    adds cargo fmt (#10)

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -94,7 +94,6 @@ pub enum Error {
     //
     // #[error(transparent)]
     // SerdeSerError(#[from] serializer_error::Error),
-
     #[cfg(feature = "wallet")]
     #[error(transparent)]
     WalletError(#[from] universal_wallet::Error),
